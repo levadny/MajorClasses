f67cd65fd514f4031009bedf9bae93de227286b2
Kostya <Kostya>
2016-06-27 20:18:24 +0300

    First commit for zerolevel in project

---
+++ src/MajorClasses.java
@@ -0,0 +1,10 @@
+
+public class MajorClasses {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Программа MajorClasses");
+        System.out.println("Программа предназначена для изучения ООП и Java");
+    }
+
+}
